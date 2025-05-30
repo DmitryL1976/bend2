485ec5eb552013c20a5b78d9fc3042cf1ac8819b
Dmitry Laabe <dmitry@it-dominanta.com>
2025-05-30 20:58:29 +0300

    123

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const http = require('http');
 const hostname = '127.0.0.1';
-const port = 3001;
+const port = 3002;
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/plain');
-res.end('Hello World 123!!!!');
+res.end('Hello World 123456!!!!');
 });
 server.listen(port, hostname, () => {
 console.log(`Server running at http://${hostname}:${port}/`);
